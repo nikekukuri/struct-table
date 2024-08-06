44598f552b6874c29ee6dd2d629051289a7bf922
nikekukuri <nikekukuri0327@gmail.com>
2024-08-06 23:48:29 +0900

    Implement: GraphQL query by use apollo/client

--- client/app/table/page.tsx
+++ client/app/table/page.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 
 import { Classes } from "@blueprintjs/core";
 import { Example, type ExampleProps } from "@blueprintjs/docs-theme";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
 import { Cell, Column, ColumnHeaderCell, Table2 } from "@blueprintjs/table";
 import './style.css'
 
@@ -43,6 +44,63 @@ function renderName(name: string) {
   );
 }
 
-export default function TableExample() {
-  return <TableDollarExample />;
+// Test
+//export default function TableExample() {
+//  return <TableDollarExample />;
+//}
+
+// Apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:8000/graphql",
+  cache: new InMemoryCache()
+});
+
+const GET_ONE_RECORD = gql`
+  query {
+    getAllColumns(id: 4) {
+      col1
+      col2
+      col3
+      col4
+    }
+  }
+`;
+
+const TestTable: React.FC = () => {
+  const { loading, error, data } = useQuery(GET_ONE_RECORD);
+
+  console.log(data);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error {error.message}</p>;
+
+  return (
+    <Table2 numRows={data.users.length}>
+      <Column
+        name="col1"
+        columnHeaderCellRenderer={() => <ColumnHeaderCell name="col1" />}
+        cellRenderer={(rowIndex: number) => <Cell>{data.users[rowIndex].col1}</Cell>}
+      />
+      <Column
+        name="col2"
+        columnHeaderCellRenderer={() => <ColumnHeaderCell name="col2" />}
+        cellRenderer={(rowIndex: number) => <Cell>{data.users[rowIndex].col2}</Cell>}
+      />
+      <Column
+        name="col3"
+        columnHeaderCellRenderer={() => <ColumnHeaderCell name="col3" />}
+        cellRenderer={(rowIndex: number) => <Cell>{data.users[rowIndex].col3}</Cell>}
+      />
+    </Table2>
+  );
 }
+
+const Tables: React.FC = () => {
+  return (
+    <ApolloProvider client={client}>
+      <TestTable />
+    </ApolloProvider>
+  );
+}
+
+export default Tables;
