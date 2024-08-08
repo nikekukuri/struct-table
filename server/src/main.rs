f488877b65758194e3e4d796fc986bc9f7a0866e
nikekukuri <nikekukuri0327@gmail.com>
2024-08-08 23:31:39 +0900

    Fix: CORS Custom option for Access-Control-Allow-Origin

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,7 @@
 use async_graphql::{http::GraphiQLSource, EmptySubscription, Schema};
 use async_graphql_rocket::{GraphQLQuery, GraphQLRequest, GraphQLResponse};
-use rocket::{http::Method, response::content, routes, State};
+use rocket::{http::{Method, Header}, response::content, routes, State, Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
 use sqlx::postgres::PgPoolOptions;
 use rocket_cors::{AllowedHeaders, AllowedOrigins, CorsOptions};
 
@@ -8,6 +9,28 @@ mod db;
 mod model;
 
 use crate::model::{mutation::MutationRoot, query::QueryRoot, TableSchema};
+
+pub struct CustomCros;
+
+#[rocket::async_trait]
+impl Fairing for CustomCros {
+    fn info(&self) -> Info {
+        Info {
+            name: "Custom CORS",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, req: &'r Request<'_>, res: &mut Response<'r>) {
+        if req.method() == rocket::http::Method::Options {
+            res.set_status(rocket::http::Status::Ok);
+        }
+        res.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        res.set_header(Header::new("Access-Control-Allow-Methods", "GET, POST, OPTIONS"));
+        res.set_header(Header::new("Access-Control-Allow-Headers", "Authorization, Content-Type"));
+    }
+}
+
 
 #[rocket::get("/")]
 fn graphiql() -> content::RawHtml<String> {
@@ -24,6 +47,11 @@ async fn graphql_request(schema: &State<TableSchema>, request: GraphQLRequest) -
     request.execute(schema.inner()).await
 }
 
+#[rocket::options("/graphql")]
+fn graphql_options() -> &'static str {
+    ""
+}
+
 #[rocket::launch]
 async fn rocket() -> _ {
     let pool = PgPoolOptions::new()
@@ -34,21 +62,22 @@ async fn rocket() -> _ {
     let scheme = Schema::build(QueryRoot, MutationRoot, EmptySubscription)
         .data(pool)
         .finish();
-    let cors_options = cors_options().to_cors().unwrap();
 
     rocket::build()
         .manage(scheme)
-        .mount("/", routes![graphiql, graphql_query, graphql_request])
-        .attach(cors_options)
+        .mount("/", routes![graphiql, graphql_query, graphql_request, graphql_options])
+        .attach(CustomCros)
 }
 
 fn cors_options() -> CorsOptions {
-    let allowed_origins = AllowedOrigins::some_exact(&["https://localhost:3001"]);
+    let allowed_origins = AllowedOrigins::some_exact(&["https://localhost:3000"]);
 
     CorsOptions {
         allowed_origins,
-        allowed_methods: vec![Method::Get, Method::Post].into_iter().map(Into::into).collect(),
+        allowed_methods: vec![Method::Get, Method::Post, Method::Options].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::all(),
         allow_credentials: true,
+        
         ..Default::default()
     }
 }
