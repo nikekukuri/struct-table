cd278b0bbe58e2d5ce2cee62b5a220005a4fd575
nikekukuri <nikekukuri0327@gmail.com>
2024-10-20 23:23:40 +0900

    Remove unused packages.

--- client/app/components/calc.test.ts
+++
@@ -1,8 +0,0 @@
-import { calcExpression } from "./calc";
-
-console.log("test", () => {
-  const exp = "1 + 2 + 3";
-  const result: number = calcExpression(exp);
-  const expected: number = 6;
-  expect(result).toBe(expected);
-});
